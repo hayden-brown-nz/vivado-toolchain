7208c21b0072ba1ccccf4c892cda5447a4b1f0a0
Hayden Brown <hayden@thebrowns.net.nz>
2018-01-26 00:02:37 +1300

    Initial commit.

---
+++ initial_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo mkdir -p /workspace; cd /workspace
+/vagrant_shared/Xilinx_Vivado_SDK_Web_2017.4_1216_1_Lin64.bin --target vivado --noexec
+sudo ./xsetup --agree 3rdPartyEULA,WebTalkTerms,XilinxEULA --batch Install --edition "Vivado HL WebPACK" --location 
+"/opt/xilinx"
