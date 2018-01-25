19e6e383edcfe109f153637af3068ab2f7ec87ca
Hayden Brown <hayden@thebrowns.net.nz>
2018-01-26 00:56:50 +1300

    Changes to Xilinx Vivado installer launch config.

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 sudo mkdir -p /workspace; cd /workspace
-/vagrant_shared/Xilinx_Vivado_SDK_Web_2017.4_1216_1_Lin64.bin --target vivado --noexec
-sudo vivado/xsetup --agree 3rdPartyEULA,WebTalkTerms,XilinxEULA --batch Install --edition "Vivado HL WebPACK" --location 
-"/opt/xilinx"
+
+sudo mkdir /opt/Xilinx
+echo "Extracting Xilinx Vivado Installer..."
+/vagrant_shared/Xilinx_Vivado_SDK_Web_2017.4_1216_1_Lin64.bin --target vivado --noexec > /dev/null
+
+echo "Installing toolchain..."
+sudo vivado/xsetup -b Install -a XilinxEULA,3rdPartyEULA,WebTalkTerms -c /vivado-toolchain/vivado-config.conf
