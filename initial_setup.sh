1881dda3eaf5aa92d04cd01d05d46d3fc8127501
Hayden Brown <hayden@thebrowns.net.nz>
2018-01-28 21:35:43 +1300

    Updated initial setup script to auto extract and install Xilinx Vivado toolchain on VM setup.

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
-sudo mkdir -p /workspace; cd /workspace
 
-sudo mkdir /opt/Xilinx
-echo "Extracting Xilinx Vivado Installer..."
-/vagrant_shared/Xilinx_Vivado_SDK_Web_2017.4_1216_1_Lin64.bin --target vivado --noexec > /dev/null
 
-echo "Installing toolchain..."
-sudo vivado/xsetup -b Install -a XilinxEULA,3rdPartyEULA,WebTalkTerms -c /vivado-toolchain/vivado-config.conf
+WORKSPACE_DIR='/workspace'
+
+VAGRANT_SHARED_DIR='/vagrant_shared'
+VIVADO_TOOLCHAIN_INSTALLER_DIR=$VAGRANT_SHARED_DIR'/vivado_installer'
+VIVADO_TOOLCHAIN_INSTALLATION_DIR="/opt/Xilinx"
+
+sudo mkdir -p $WORKSPACE_DIR; # sudo chown vagrant:vagrant $WORKSPACE_DIR;
+cd $WORKSPACE_DIR
+
+
+if [ ! -d  $VIVADO_TOOLCHAIN_INSTALLER_DIR ] ; then
+    echo "Extracting Xilinx Vivado Installer..."
+    mkdir -p $VIVADO_TOOLCHAIN_INSTALLER_DIR
+    tar -xvf /vagrant_shared/vivado_installer.tgz -C $VAGRANT_SHARED_DIR
+fi
+
+if [ ! -d  $VIVADO_TOOLCHAIN_INSTALLATION_DIR ] ; then
+    echo "Installing Xilinx Vivado toolchain..."
+    mkdir -p $VIVADO_TOOLCHAIN_INSTALLATION_DIR
+    $VIVADO_TOOLCHAIN_INSTALLER_DIR/xsetup -b Install -a XilinxEULA,3rdPartyEULA,WebTalkTerms -c /vivado-toolchain/vivado-config.conf
+fi
+
